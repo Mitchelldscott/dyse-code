817d703b8b10c0ada82cefd458d926e0b67cf167
Mitchell Scott <misc4432@colorado.edu>
2023-10-27 11:12:00 -0600

    need better missing route fix

--- dysepy/dyse.bash
+++ dysepy/dyse.bash
@@ -110,7 +110,6 @@ fi
 #################### Network setup ####################
 
 ${SUDO} ifconfig lo multicast
-${SUDO} route delete -net 224.0.0.0 netmask 240.0.0.0 dev lo
 ${SUDO} route add -net 224.0.0.0 netmask 240.0.0.0 dev lo
 
 
